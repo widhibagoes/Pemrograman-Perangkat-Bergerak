9b7d9d42ebb1c7ebc80397d3b37866bc5cd9a198
widhibagoes <widhi.bagoes.wb@gmail.com>
2019-12-31 09:27:54 +0700

    Pemrograman Mobile

---
+++ MyClassIK2B/app/src/main/java/widhi/project/myclass_ik_2b/Informasi.java
@@ -0,0 +1,14 @@
+package widhi.project.myclass_ik_2b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Informasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi);
+    }
+}
